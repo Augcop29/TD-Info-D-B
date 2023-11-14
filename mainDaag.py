e8c78bb39e7d7438a3c55e3da3d9e82974ba9f1c
Augustin COPPENS <augustin.coppens@gmail.com>
2023-11-14 10:17:09 +0100

    Reduction Taille Image + Code Daag

--- mainDaag.py
+++ mainDaag.py
@@ -1,1 +1,21 @@
-print("hello world")
+from PIL import Image
+from IPython.display import display
+
+im = Image.open("IMG_3135.png")
+im = im.convert("RGB")
+px = im.load()
+
+w,h = im.size
+
+def list_couleur(px,w,h):
+    lst = {}
+    for x in range (w):
+        for y in range (h):
+            if px[x,y] in lst:
+                lst[px[x,y]] += 1
+            else:
+                lst[px[x,y]] = 1
+    return lst
+            
+
+list_couleur(px,w,h)
